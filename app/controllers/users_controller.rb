cd3f04007e299f775c9b117b801924a586084cfa
Shanks <onyangogeorge954@gmail.com>
2022-12-05 23:51:16 +0300

    add create resource

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   protect_from_forgery with: :null_session
+
+  def index
+    users = User.all
+    render json: users
+  end
+
   def show
     user = User.find_by(id: session[:user_id])
     if user
@@ -7,5 +13,21 @@ class UsersController < ApplicationController
     else
       render json: { error: "Not authorized" }, status: :unauthorized
     end
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def user_params
+    params.permit(:email, :username, :location, :phone_number, :professional_status)
   end
 end
